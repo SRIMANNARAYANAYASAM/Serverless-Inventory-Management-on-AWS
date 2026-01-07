1df5444dd361ca8f2a4f7d6e0978e3be38abbaf1
Sriman <srimannarayana.yasam@gmail.com>
2026-01-06 21:23:46 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bhanudas-terraform-state-bucket"
+    bucket = "srimannarayana-serverless-terraform-state-bucket"
     key    = "ha-aws-arch/terraform.tfstate"
     region = "us-east-2"
   }
